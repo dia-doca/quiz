6caf5caf864153bed64e350abd4e7eeaa8cedd8d
Ivan Druzhinin <DIA-Doca@MacBook-Pro-mak.local>
2020-11-02 11:40:05 +0300

    code review v1

---
+++ codereview.swift
@@ -0,0 +1,48 @@
+/// Please imagine your regular work's day and make code review
+
+import UIKit
+
+protocol ReaderDelegate: class {
+    func didReadData(data: Data)
+}
+
+class Reader{
+    var file: String!
+    var output: ReaderDelegate?
+    var readCompleteBlock: (() -> Void)?
+
+    func read() {
+        let fileUrl = URL(fileURLWithPath: file!)
+        let data = try? Data(contentsOf: fileUrl)
+        self.output?.didReadData(data: data!)
+        self.readCompleteBlock?();
+    }
+}
+
+class orderReader: ReaderDelegate {
+    public var reader: Reader
+    init(_ file: URL) {
+        self.reader = Reader()
+        self.reader.file = file.absoluteString.replacingOccurrences(of: "file://", with: "")
+        self.reader.output = self
+        self.reader.readCompleteBlock = {
+            self.didComplete()
+        }
+    }
+
+    func Read() {
+        self.reader.read()
+    }
+
+    func didComplete() {
+        print("end of file")
+    }
+
+    func didReadData(data: Data) {
+        print("\(data)")
+    }
+}
+
+let filePath = Bundle.main.path(forResource: "myOrders.csv", ofType: nil)
+let orderReader = orderReader(URL(fileURLWithPath: filePath!))
+orderReader.Read()
